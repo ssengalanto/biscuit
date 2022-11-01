cf491f90a7f81fc48b21317f639eb5618b56b2a2
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-01 16:45:51 +0800

    create initial account service packages with doc comments

---
+++ cmd/account/internal/infrastructure/doc.go
@@ -0,0 +1,8 @@
+// Copyright 2022 Ssen Galanto. All rights reserved.
+
+/*
+Package infrastructure is a layer that holds everything that interacts with other systems - Secondary/Driven Adapters.
+Secondary adapters wrap around a tool and adapt its input/output to a port,
+which fits the application core needs and can simply be an interface or a complex set of interfaces and objects.
+*/
+package infrastructure
