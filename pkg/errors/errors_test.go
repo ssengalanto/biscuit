7e6def0c486b4605750da4b83fb93adc4af3d052
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-11 12:33:24 +0800

    update errors_test to use testify require package instead of assert

--- pkg/errors/errors_test.go
+++ pkg/errors/errors_test.go
@@ -6,22 +6,22 @@ import (
 	"testing"
 
 	apperr "github.com/ssengalanto/potato-project/pkg/errors"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestNew(t *testing.T) {
 	err := apperr.New("test error")
-	assert.NotNil(t, err, "error shouldn't be nil")
+	require.NotNil(t, err, "error shouldn't be nil")
 }
 
 func TestError(t *testing.T) {
 	msg := "test error"
 	err := apperr.New(msg)
-	assert.EqualError(t, err, msg, "error should have the same error message")
+	require.EqualError(t, err, msg, "error should have the same error message")
 }
 
 func TestWrap(t *testing.T) {
 	err := apperr.Wrap(fmt.Errorf("test error: %w", apperr.ErrInternal))
-	assert.NotNil(t, err, "error shouldn't be nil")
-	assert.True(t, errors.Is(err, apperr.ErrInternal), "error is not internal")
+	require.NotNil(t, err, "error shouldn't be nil")
+	require.True(t, errors.Is(err, apperr.ErrInternal), "error is not internal")
 }
