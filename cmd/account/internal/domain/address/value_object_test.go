fe8c07ea4e5586a0f36214fa6557d56cb6d8f7ac
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-16 11:25:42 +0800

    create tests for newly added methods in address value objects

--- cmd/account/internal/domain/address/value_object_test.go
+++ cmd/account/internal/domain/address/value_object_test.go
@@ -53,17 +53,8 @@ func TestComponents_IsValid(t *testing.T) {
 			},
 		},
 		{
-			name: "invalid address components",
-			payload: address.Components{
-				PlaceID:          "",
-				AddressLine1:     address.Names{},
-				AddressLine2:     address.Names{},
-				City:             address.Names{},
-				State:            address.Names{},
-				Country:          address.Names{},
-				PostalCode:       address.Names{},
-				FormattedAddress: "",
-			},
+			name:    "invalid address components",
+			payload: address.Components{},
 			assert: func(t *testing.T, result bool, err error) {
 				errMsg := fmt.Sprintf("address components should be invalid: %s", err)
 				require.False(t, result, errMsg)
@@ -76,6 +67,104 @@ func TestComponents_IsValid(t *testing.T) {
 			components := tc.payload
 			ok, err := components.IsValid()
 			tc.assert(t, ok, err)
+		})
+	}
+}
+
+func TestAddress_Update(t *testing.T) {
+	current := gofakeit.Address()
+	update := gofakeit.Address()
+
+	currentAddr := address.Components{
+		PlaceID: gofakeit.UUID(),
+		AddressLine1: address.Names{
+			ShortName: current.Street,
+			LongName:  current.Street,
+		},
+		AddressLine2: address.Names{
+			ShortName: current.Street,
+			LongName:  current.Street,
+		},
+		City: address.Names{
+			ShortName: current.City,
+			LongName:  current.City,
+		},
+		State: address.Names{
+			ShortName: current.State,
+			LongName:  current.State,
+		},
+		Country: address.Names{
+			ShortName: current.Country,
+			LongName:  current.Country,
+		},
+		PostalCode: address.Names{
+			ShortName: current.Zip,
+			LongName:  current.Zip,
+		},
+		FormattedAddress: current.Address,
+	}
+
+	testCases := []struct {
+		name    string
+		current address.Components
+		update  address.Components
+		assert  func(t *testing.T, expected address.Components, actual address.Components, err error)
+	}{
+		{
+			name:    "update address components success",
+			current: currentAddr,
+			update: address.Components{
+				PlaceID: gofakeit.UUID(),
+				AddressLine1: address.Names{
+					ShortName: update.Street,
+					LongName:  update.Street,
+				},
+				AddressLine2: address.Names{
+					ShortName: update.Street,
+					LongName:  update.Street,
+				},
+				City: address.Names{
+					ShortName: update.City,
+					LongName:  update.City,
+				},
+				State: address.Names{
+					ShortName: update.State,
+					LongName:  update.State,
+				},
+				Country: address.Names{
+					ShortName: update.Country,
+					LongName:  update.Country,
+				},
+				PostalCode: address.Names{
+					ShortName: update.Zip,
+					LongName:  update.Zip,
+				},
+				FormattedAddress: update.Address,
+			},
+			assert: func(t *testing.T, expected address.Components, actual address.Components, err error) {
+				errMsg := fmt.Sprintf("update address components should succeed: %s", err)
+				require.Equal(t, expected, actual, errMsg)
+				require.Nil(t, err, errMsg)
+			},
+		},
+		{
+			name:    "update address components failed",
+			current: currentAddr,
+			update: address.Components{
+				PlaceID: gofakeit.UUID(),
+			},
+			assert: func(t *testing.T, expected address.Components, actual address.Components, err error) {
+				errMsg := fmt.Sprintf("update address components should fail: %s", err)
+				require.NotEqual(t, expected, actual, errMsg)
+				require.NotNil(t, err, errMsg)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			components := tc.current
+			newComponents, err := components.Update(tc.update)
+			tc.assert(t, tc.update, newComponents, err)
 		})
 	}
 }
@@ -99,11 +188,8 @@ func TestGeometry_IsValid(t *testing.T) {
 			},
 		},
 		{
-			name: "invalid address geometry",
-			payload: address.Geometry{
-				Lat: 0,
-				Lng: 0,
-			},
+			name:    "invalid address geometry",
+			payload: address.Geometry{},
 			assert: func(t *testing.T, result bool, err error) {
 				errMsg := fmt.Sprintf("address geometry should be invalid: %s", err)
 				require.False(t, result, errMsg)
@@ -116,6 +202,55 @@ func TestGeometry_IsValid(t *testing.T) {
 			geometry := tc.payload
 			ok, err := geometry.IsValid()
 			tc.assert(t, ok, err)
+		})
+	}
+}
+
+func TestGeometry_Update(t *testing.T) {
+	current := address.Geometry{
+		Lat: gofakeit.Latitude(),
+		Lng: gofakeit.Longitude(),
+	}
+	update := address.Geometry{
+		Lat: gofakeit.Latitude(),
+		Lng: gofakeit.Longitude(),
+	}
+
+	testCases := []struct {
+		name    string
+		current address.Geometry
+		update  address.Geometry
+		assert  func(t *testing.T, expected address.Geometry, actual address.Geometry, err error)
+	}{
+		{
+			name:    "update address geometry success",
+			current: current,
+			update:  update,
+			assert: func(t *testing.T, expected address.Geometry, actual address.Geometry, err error) {
+				errMsg := fmt.Sprintf("update address geometry should succeed: %s", err)
+				require.Equal(t, expected, actual, errMsg)
+				require.Nil(t, err, errMsg)
+			},
+		},
+		{
+			name:    "update address geometry failed",
+			current: current,
+			update: address.Geometry{
+				Lat: gofakeit.Longitude(),
+				Lng: 0,
+			},
+			assert: func(t *testing.T, expected address.Geometry, actual address.Geometry, err error) {
+				errMsg := fmt.Sprintf("update address geometry should fail: %s", err)
+				require.NotEqual(t, expected, actual, errMsg)
+				require.NotNil(t, err, errMsg)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			components := tc.current
+			newComponents, err := components.Update(tc.update)
+			tc.assert(t, tc.update, newComponents, err)
 		})
 	}
 }
