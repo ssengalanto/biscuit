fd8e5ef38723b8875eb6773b9efdf40bbe24b049
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-19 21:51:50 +0800

    implement update person details method for person entity

--- cmd/account/internal/domain/person/entity.go
+++ cmd/account/internal/domain/person/entity.go
@@ -1,6 +1,8 @@
 package person
 
 import (
+	"time"
+
 	"github.com/google/uuid"
 	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/address"
 )
@@ -14,9 +16,50 @@ type Entity struct {
 	Address   *address.Entity `json:"address"`
 }
 
+// UpdateDetailsInput - input for updating peron Details.
+type UpdateDetailsInput struct {
+	FirstName   *string
+	LastName    *string
+	Email       *string
+	Phone       *string
+	DateOfBirth *time.Time
+}
+
 // New creates a new person entity.
 func New() Entity {
 	return Entity{
 		ID: uuid.New(),
 	}
+}
+
+func (e *Entity) UpdateDetails(input UpdateDetailsInput) error {
+	details := e.Details
+
+	if input.FirstName != nil {
+		details.FirstName = *input.FirstName
+	}
+
+	if input.LastName != nil {
+		details.LastName = *input.LastName
+	}
+
+	if input.Email != nil {
+		details.Email = *input.Email
+	}
+
+	if input.Phone != nil {
+		details.Phone = *input.Phone
+	}
+
+	if input.DateOfBirth != nil {
+		details.DateOfBirth = *input.DateOfBirth
+	}
+
+	newDetails, err := e.Details.Update(details)
+	if err != nil {
+		return err
+	}
+
+	e.Details = newDetails
+	return nil
 }
