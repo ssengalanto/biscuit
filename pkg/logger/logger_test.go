57786e162a101f7eb6f3b30232d3202d63cbb919
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-22 14:07:08 +0800

    update tests in logger package

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -1,9 +1,11 @@
 package logger_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
+	"github.com/ssengalanto/potato-project/pkg/constants"
 	"github.com/ssengalanto/potato-project/pkg/logger"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -11,9 +13,36 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	log, err := logger.New()
-	require.NotNil(t, log)
-	require.Nil(t, err)
+	testCases := []struct {
+		name   string
+		env    string
+		assert func(t *testing.T, result *logger.Logger, err error)
+	}{
+		{
+			name: "valid env",
+			env:  constants.Dev,
+			assert: func(t *testing.T, result *logger.Logger, err error) {
+				errMsg := fmt.Sprintf("creating new instance should succeed: %s", err)
+				require.NotNil(t, result, errMsg)
+				require.Nil(t, err, errMsg)
+			},
+		},
+		{
+			name: "invalid env",
+			env:  "invalid",
+			assert: func(t *testing.T, result *logger.Logger, err error) {
+				errMsg := fmt.Sprintf("creating new instance should fail: %s", err)
+				require.Nil(t, result, errMsg)
+				require.NotNil(t, err, errMsg)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			log, err := logger.New(tc.env)
+			tc.assert(t, log, err)
+		})
+	}
 }
 
 func TestLogger_Info(t *testing.T) {
