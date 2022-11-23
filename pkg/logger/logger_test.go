3dfeab6b021ea2e1d0ede9aca0550f8b6e0a38e0
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-11-23 12:16:54 +0800

    create tests for default logger

---
+++ pkg/logger/logger_test.go
@@ -0,0 +1,57 @@
+package logger_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/ssengalanto/potato-project/pkg/constants"
+	"github.com/ssengalanto/potato-project/pkg/interfaces"
+	"github.com/ssengalanto/potato-project/pkg/logger"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNew(t *testing.T) {
+	testCases := []struct {
+		name    string
+		env     string
+		logType string
+		assert  func(t *testing.T, result interfaces.Logger, err error)
+	}{
+		{
+			name:    "valid env",
+			env:     constants.Dev,
+			logType: constants.ZapLogType,
+			assert: func(t *testing.T, result interfaces.Logger, err error) {
+				errMsg := fmt.Sprintf("creating new instance should succeed: %s", err)
+				require.NotNil(t, result, errMsg)
+				require.Nil(t, err, errMsg)
+			},
+		},
+		{
+			name:    "invalid env",
+			env:     "invalid",
+			logType: constants.ZapLogType,
+			assert: func(t *testing.T, result interfaces.Logger, err error) {
+				errMsg := fmt.Sprintf("creating new instance should fail: %s", err)
+				require.Nil(t, result, errMsg)
+				require.NotNil(t, err, errMsg)
+			},
+		},
+		{
+			name:    "invalid log type",
+			env:     constants.Dev,
+			logType: "invalid",
+			assert: func(t *testing.T, result interfaces.Logger, err error) {
+				errMsg := fmt.Sprintf("creating new instance should fail: %s", err)
+				require.Nil(t, result, errMsg)
+				require.NotNil(t, err, errMsg)
+			},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			log, err := logger.New(tc.env, tc.logType)
+			tc.assert(t, log, err)
+		})
+	}
+}
