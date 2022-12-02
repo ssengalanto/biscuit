5ebc4f22d2ff5c336d712a71ba6992aad4c03d7e
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-02 16:16:04 +0800

    implement find by id query in account repository

--- cmd/account/internal/infrastructure/persistence/pgsql/account_repository.go
+++ cmd/account/internal/infrastructure/persistence/pgsql/account_repository.go
@@ -3,6 +3,7 @@ package pgsql
 import (
 	"context"
 
+	"github.com/google/uuid"
 	"github.com/jmoiron/sqlx"
 	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/account"
 )
@@ -50,12 +51,37 @@ func (a *AccountRepository) Save(ctx context.Context, entity account.Entity) (ac
 	return acc.ToEntity(), nil
 }
 
+// FindByID get an account record with specific ID in the database.
+func (a *AccountRepository) FindByID(ctx context.Context, id uuid.UUID) (account.Entity, error) {
+	acc := Account{}
+
+	stmt, err := a.db.PreparexContext(ctx, AccountQueries["findByID"])
+	if err != nil {
+		return account.Entity{}, err
+	}
+
+	row := stmt.QueryRowxContext(ctx, id)
+
+	err = row.StructScan(&acc)
+	if err != nil {
+		return acc.ToEntity(), err
+	}
+
+	return acc.ToEntity(), nil
+}
+
 // AccountQueries is a map holds all queries for account table.
 var AccountQueries = map[string]string{ //nolint:gochecknoglobals //intended
-	"save": saveAccountQuery,
+	"save":     saveAccountQuery,
+	"findByID": findByIDQuery,
 }
 
 const saveAccountQuery = `
 	INSERT INTO account (id, email, password, active, last_login_at)
 	VALUES ($1, $2, $3, $4, $5)
 	RETURNING *`
+
+const findByIDQuery = `
+	SELECT id, email, password, active, last_login_at
+	FROM account
+	WHERE id = $1`
