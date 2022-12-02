63e72fba43701bcceb10b93c825261d5424e114f
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-02 16:14:11 +0800

    update account repository interface

--- cmd/account/internal/domain/account/repository.go
+++ cmd/account/internal/domain/account/repository.go
@@ -4,9 +4,9 @@ import "context"
 
 type Repository interface {
 	Save(ctx context.Context, entity Entity) (Entity, error)
-	FindOneById(ctx context.Context, id string) (Entity, error)
-	FindOneByEmail(ctx context.Context, email string) (Entity, error)
+	FindByID(ctx context.Context, id string) (Entity, error)
+	FindByEmail(ctx context.Context, email string) (Entity, error)
 	DeactivateAccount(ctx context.Context, id string) (Entity, error)
 	ActivateAccount(ctx context.Context, id string) (Entity, error)
-	DeleteOneById(ctx context.Context, id string) (Entity, error)
+	DeleteById(ctx context.Context, id string) (Entity, error)
 }
