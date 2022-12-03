f960e6d783588136ad7db5d40dd12cddb0e05f86
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-03 19:30:51 +0800

    create tests for update by id query in account repository

--- cmd/account/internal/infrastructure/persistence/pgsql/account_repository_test.go
+++ cmd/account/internal/infrastructure/persistence/pgsql/account_repository_test.go
@@ -101,6 +101,38 @@ func TestAccountRepository_FindByEmail(t *testing.T) {
 	require.Equal(t, result, entity)
 }
 
+func TestAccountRepository_UpdateById(t *testing.T) {
+	entity := newAccountEntity()
+	update := newAccountEntity()
+	update.ID = entity.ID
+
+	db, dbmock, err := mock.NewSqlDb()
+	require.NoError(t, err)
+	defer db.Close()
+
+	repo := pgsql.NewAccountRepository(db)
+
+	query, ok := pgsql.AccountQueries["updateByID"]
+	require.True(t, ok)
+
+	rows := sqlmock.NewRows([]string{"id", "email", "password", "active", "last_login_at"}).
+		AddRow(entity.ID, entity.Email, entity.Password, entity.Active, entity.LastLoginAt)
+
+	dbmock.ExpectBegin()
+	stmt := dbmock.ExpectPrepare(regexp.QuoteMeta(query))
+	stmt.ExpectQuery().WithArgs(
+		update.ID,
+		update.Email,
+		update.Password,
+		update.Active,
+		update.LastLoginAt).WillReturnRows(rows)
+	dbmock.ExpectCommit()
+
+	result, err := repo.UpdateById(context.Background(), update)
+	require.NoError(t, err)
+	require.Equal(t, result, entity)
+}
+
 func newAccountEntity() account.Entity {
 	return account.Entity{
 		ID:          uuid.New(),
