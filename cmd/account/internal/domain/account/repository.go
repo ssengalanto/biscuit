242a8b35517da6dc8896f82acc533841e1406651
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-03 20:49:32 +0800

    add exists method in account repository interface

--- cmd/account/internal/domain/account/repository.go
+++ cmd/account/internal/domain/account/repository.go
@@ -2,10 +2,12 @@ package account
 
 import (
 	"context"
+
 	"github.com/google/uuid"
 )
 
 type Repository interface {
+	Exists(ctx context.Context, id uuid.UUID) (bool, error)
 	Create(ctx context.Context, entity Entity) (Entity, error)
 	FindByID(ctx context.Context, id uuid.UUID) (Entity, error)
 	FindByEmail(ctx context.Context, email string) (Entity, error)
