3f931fcfc971619b6f04ab9b2895e1d30c0b9fa8
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-03 19:30:37 +0800

    implement update by id query in account repository

--- cmd/account/internal/infrastructure/persistence/pgsql/account_repository.go
+++ cmd/account/internal/infrastructure/persistence/pgsql/account_repository.go
@@ -89,11 +89,46 @@ func (a *AccountRepository) FindByEmail(ctx context.Context, email string) (acco
 	return acc.ToEntity(), nil
 }
 
+// UpdateById updates an account record with specific id in the database.
+func (a *AccountRepository) UpdateById(ctx context.Context, entity account.Entity) (account.Entity, error) {
+	acc := Account{}
+
+	tx := a.db.MustBeginTx(ctx, nil)
+	defer tx.Rollback() //nolint:errcheck //unnecessary
+
+	stmt, err := tx.PreparexContext(ctx, AccountQueries["updateByID"])
+	if err != nil {
+		return account.Entity{}, err
+	}
+
+	row := stmt.QueryRowxContext(
+		ctx,
+		entity.ID,
+		entity.Email,
+		entity.Password,
+		entity.Active,
+		entity.LastLoginAt,
+	)
+
+	err = row.StructScan(&acc)
+	if err != nil {
+		return acc.ToEntity(), err
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return acc.ToEntity(), err
+	}
+
+	return acc.ToEntity(), nil
+}
+
 // AccountQueries is a map holds all queries for account table.
 var AccountQueries = map[string]string{ //nolint:gochecknoglobals //intended
 	"save":        saveAccountQuery,
 	"findByID":    findByIDQuery,
 	"findByEmail": findByEmailQuery,
+	"updateByID":  updateByIDQuery,
 }
 
 const saveAccountQuery = `
@@ -110,3 +145,10 @@ const findByEmailQuery = `
 	SELECT id, email, password, active, last_login_at
 	FROM account
 	WHERE email = $1`
+
+const updateByIDQuery = `
+	UPDATE account
+	SET email = $2, password = $3, active = $4, last_login_at = $5, updated_at = NOW()
+	FROM account
+	WHERE id = $1
+	RETURNING *`
