be4852593e495e8e6f2d5b5fb696d967b314302e
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-08 12:35:17 +0800

    create namesJSON type and implement mustDecodeJSON method

--- cmd/account/internal/infrastructure/persistence/pgsql/address.go
+++ cmd/account/internal/infrastructure/persistence/pgsql/address.go
@@ -1,28 +1,41 @@
 package pgsql
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/google/uuid"
 	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/address"
 )
 
+type namesJSON string
+
+func (n namesJSON) mustDecodeJSON() address.Names {
+	var names address.Names
+	err := json.Unmarshal([]byte(n), &names)
+	if err != nil {
+		panic(err)
+	}
+
+	return names
+}
+
 // Address pgsql model.
 type Address struct {
-	ID               uuid.UUID     `json:"id" db:"id"`
-	PersonID         uuid.UUID     `json:"personId" db:"person_id"`
-	PlaceID          string        `json:"placeId" db:"place_id"`
-	AddressLine1     address.Names `json:"addressLine1" db:"address_line1"`
-	AddressLine2     address.Names `json:"addressLine2" db:"address_line2"`
-	City             address.Names `json:"city" db:"city"`
-	State            address.Names `json:"state" db:"state"`
-	Country          address.Names `json:"country" db:"country"`
-	PostalCode       address.Names `json:"postalCode" db:"postal_code"`
-	FormattedAddress string        `json:"formattedAddress" db:"formatted_address"`
-	Lat              float64       `json:"lat" db:"lat"`
-	Lng              float64       `json:"lng" db:"lng"`
-	CreatedAt        time.Time     `json:"createdAt" db:"created_at"`
-	UpdatedAt        time.Time     `json:"updatedAt" db:"updated_at"`
+	ID               uuid.UUID `json:"id" db:"id"`
+	PersonID         uuid.UUID `json:"personId" db:"person_id"`
+	PlaceID          string    `json:"placeId" db:"place_id"`
+	AddressLine1     namesJSON `json:"addressLine1" db:"address_line1"`
+	AddressLine2     namesJSON `json:"addressLine2" db:"address_line2"`
+	City             namesJSON `json:"city" db:"city"`
+	State            namesJSON `json:"state" db:"state"`
+	Country          namesJSON `json:"country" db:"country"`
+	PostalCode       namesJSON `json:"postalCode" db:"postal_code"`
+	FormattedAddress string    `json:"formattedAddress" db:"formatted_address"`
+	Lat              float64   `json:"lat" db:"lat"`
+	Lng              float64   `json:"lng" db:"lng"`
+	CreatedAt        time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt        time.Time `json:"updatedAt" db:"updated_at"`
 }
 
 // ToEntity transforms the address model to account entity.
@@ -32,12 +45,12 @@ func (a Address) ToEntity() address.Entity {
 		PersonID: a.PersonID,
 		Components: address.Components{
 			PlaceID:          a.PlaceID,
-			AddressLine1:     a.AddressLine1,
-			AddressLine2:     a.AddressLine2,
-			City:             a.City,
-			State:            a.State,
-			Country:          a.Country,
-			PostalCode:       a.PostalCode,
+			AddressLine1:     a.AddressLine1.mustDecodeJSON(),
+			AddressLine2:     a.AddressLine2.mustDecodeJSON(),
+			City:             a.City.mustDecodeJSON(),
+			State:            a.State.mustDecodeJSON(),
+			Country:          a.Country.mustDecodeJSON(),
+			PostalCode:       a.PostalCode.mustDecodeJSON(),
 			FormattedAddress: a.FormattedAddress,
 		},
 		Geometry: address.Geometry{
