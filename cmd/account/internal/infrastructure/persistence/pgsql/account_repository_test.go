021baa49a4cd95870f4a9f55f4bd85a55bcaf719
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-09 14:15:32 +0800

    update tests for find by id in account repository

--- cmd/account/internal/infrastructure/persistence/pgsql/account_repository_test.go
+++ cmd/account/internal/infrastructure/persistence/pgsql/account_repository_test.go
@@ -6,11 +6,7 @@ import (
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
-	"github.com/brianvoe/gofakeit/v6"
 	"github.com/google/uuid"
-	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/account"
-	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/address"
-	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/person"
 	"github.com/ssengalanto/potato-project/cmd/account/internal/infrastructure/persistence/pgsql"
 	"github.com/ssengalanto/potato-project/pkg/mock"
 	"github.com/stretchr/testify/require"
@@ -80,8 +76,7 @@ func TestAccountRepository_Create(t *testing.T) {
 
 	dbmock.ExpectBegin()
 
-	accountRows := sqlmock.NewRows([]string{"id", "email", "password", "active", "last_login_at"}).
-		AddRow(entity.ID, entity.Email, entity.Password, entity.Active, entity.LastLoginAt)
+	accountRow := createAccountRow(entity)
 	createAccountQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryCreateAccount)
 	accountStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(createAccountQuery))
 	accountStmt.ExpectQuery().WithArgs(
@@ -89,21 +84,9 @@ func TestAccountRepository_Create(t *testing.T) {
 		entity.Email,
 		entity.Password,
 		entity.Active,
-		entity.LastLoginAt).WillReturnRows(accountRows)
+		entity.LastLoginAt).WillReturnRows(accountRow)
 
-	personRows := sqlmock.NewRows(
-		[]string{"id", "account_id", "first_name", "last_name", "email", "phone", "date_of_birth", "avatar"},
-	).
-		AddRow(
-			entity.Person.ID,
-			entity.Person.AccountID,
-			entity.Person.Details.FirstName,
-			entity.Person.Details.LastName,
-			entity.Person.Details.Email,
-			entity.Person.Details.Phone,
-			entity.Person.Details.DateOfBirth,
-			entity.Person.Avatar,
-		)
+	personRow := createPersonRow(entity)
 	createPersonQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryCreatePerson)
 	personStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(createPersonQuery))
 	personStmt.ExpectQuery().WithArgs(
@@ -115,42 +98,13 @@ func TestAccountRepository_Create(t *testing.T) {
 		entity.Person.Details.Phone,
 		entity.Person.Details.DateOfBirth,
 		entity.Person.Avatar,
-	).WillReturnRows(personRows)
+	).WillReturnRows(personRow)
 
 	createAddressQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryCreateAddress)
 	addressStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(createAddressQuery))
 
 	for _, addr := range *entity.Person.Address {
-		addressRows := sqlmock.NewRows(
-			[]string{
-				"id",
-				"person_id",
-				"place_id",
-				"address_line1",
-				"address_line2",
-				"city",
-				"state",
-				"country",
-				"postal_code",
-				"formatted_address",
-				"lat",
-				"lng",
-			},
-		).
-			AddRow(
-				addr.ID,
-				addr.PersonID,
-				addr.Components.PlaceID,
-				addr.Components.AddressLine1.MustEncodeJSON(),
-				addr.Components.AddressLine2.MustEncodeJSON(),
-				addr.Components.City.MustEncodeJSON(),
-				addr.Components.State.MustEncodeJSON(),
-				addr.Components.Country.MustEncodeJSON(),
-				addr.Components.PostalCode.MustEncodeJSON(),
-				addr.Components.FormattedAddress,
-				addr.Geometry.Lat,
-				addr.Geometry.Lng,
-			)
+		addressRows := createAddressRow(addr)
 		addressStmt.ExpectQuery().WithArgs(
 			addr.ID,
 			addr.PersonID,
@@ -184,12 +138,24 @@ func TestAccountRepository_FindByID(t *testing.T) {
 
 	repo := pgsql.NewAccountRepository(db)
 
-	rows := sqlmock.NewRows([]string{"id", "email", "password", "active", "last_login_at"}).
-		AddRow(entity.ID, entity.Email, entity.Password, entity.Active, entity.LastLoginAt)
+	dbmock.ExpectBegin()
 
-	query := pgsql.MustBeValidAccountQuery(pgsql.QueryFindByID)
-	stmt := dbmock.ExpectPrepare(regexp.QuoteMeta(query))
-	stmt.ExpectQuery().WithArgs(entity.ID).WillReturnRows(rows)
+	accountRow := createAccountRow(entity)
+	findAccountByIDQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryFindAccountByID)
+	accountStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(findAccountByIDQuery))
+	accountStmt.ExpectQuery().WithArgs(entity.ID).WillReturnRows(accountRow)
+
+	personRow := createPersonRow(entity)
+	createPersonQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryFindPersonByAccountID)
+	personStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(createPersonQuery))
+	personStmt.ExpectQuery().WithArgs(entity.ID).WillReturnRows(personRow)
+
+	createAddressQuery := pgsql.MustBeValidAccountQuery(pgsql.QueryFindAddressByPersonID)
+	addressStmt := dbmock.ExpectPrepare(regexp.QuoteMeta(createAddressQuery))
+	addressRows := createAddressRows(entity)
+	addressStmt.ExpectQuery().WithArgs(entity.Person.ID).WillReturnRows(addressRows)
+
+	dbmock.ExpectCommit()
 
 	result, err := repo.FindByID(context.Background(), entity.ID)
 	require.NoError(t, err)
@@ -264,74 +230,4 @@ func TestAccountRepository_DeleteByID(t *testing.T) {
 	result, err := repo.DeleteByID(context.Background(), entity.ID)
 	require.NoError(t, err)
 	require.Equal(t, result, entity)
-}
-
-func newAccountEntity() account.Entity {
-	accountID := uuid.New()
-	personID := uuid.New()
-
-	return account.Entity{
-		ID:          accountID,
-		Email:       account.Email(gofakeit.Email()),
-		Password:    account.Password(gofakeit.Password(true, true, true, true, false, 10)),
-		Active:      gofakeit.Bool(),
-		LastLoginAt: gofakeit.Date(),
-		Person: &person.Entity{
-			ID:        personID,
-			AccountID: accountID,
-			Details: person.Details{
-				FirstName:   gofakeit.FirstName(),
-				LastName:    gofakeit.LastName(),
-				Email:       gofakeit.Email(),
-				Phone:       gofakeit.Phone(),
-				DateOfBirth: gofakeit.Date(),
-			},
-			Avatar: person.Avatar(gofakeit.URL()),
-			Address: &[]address.Entity{
-				newAddressEntity(personID),
-				newAddressEntity(personID),
-			},
-		},
-	}
-}
-
-func newAddressEntity(personID uuid.UUID) address.Entity {
-	addr := gofakeit.Address()
-
-	return address.Entity{
-		ID:       uuid.New(),
-		PersonID: personID,
-		Components: address.Components{
-			PlaceID: gofakeit.UUID(),
-			AddressLine1: address.Names{
-				ShortName: addr.Street,
-				LongName:  addr.Street,
-			},
-			AddressLine2: address.Names{
-				ShortName: addr.Street,
-				LongName:  addr.Street,
-			},
-			City: address.Names{
-				ShortName: addr.City,
-				LongName:  addr.City,
-			},
-			State: address.Names{
-				ShortName: addr.State,
-				LongName:  addr.State,
-			},
-			Country: address.Names{
-				ShortName: addr.Country,
-				LongName:  addr.Country,
-			},
-			PostalCode: address.Names{
-				ShortName: addr.Zip,
-				LongName:  addr.Zip,
-			},
-			FormattedAddress: addr.Address,
-		},
-		Geometry: address.Geometry{
-			Lat: gofakeit.Latitude(),
-			Lng: gofakeit.Longitude(),
-		},
-	}
 }
