7d60552282200a393d911ed37c84745d4c75c93d
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-12 10:46:29 +0800

    update account repository interface

--- cmd/account/internal/domain/account/repository.go
+++ cmd/account/internal/domain/account/repository.go
@@ -11,6 +11,6 @@ type Repository interface {
 	Create(ctx context.Context, entity Entity) (Entity, error)
 	FindByID(ctx context.Context, id uuid.UUID) (Entity, error)
 	FindByEmail(ctx context.Context, email string) (Entity, error)
-	UpdateByID(ctx context.Context, entity Entity) (Entity, error)
+	Update(ctx context.Context, entity Entity) (Entity, error)
 	DeleteByID(ctx context.Context, id uuid.UUID) (Entity, error)
 }
