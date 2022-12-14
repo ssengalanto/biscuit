e93b9279c6dd189b1944c0f3b330124735c752d8
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-14 13:29:58 +0800

    create happy path tests for encoding and decoding json

---
+++ pkg/http/response/json/json_test.go
@@ -0,0 +1,66 @@
+package json_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	httpjson "github.com/ssengalanto/potato-project/pkg/http/response/json"
+)
+
+func TestEncodeResponse(t *testing.T) {
+	user := newTestUser()
+
+	w := httptest.NewRecorder()
+	err := httpjson.EncodeResponse(w, http.StatusOK, user)
+	require.NoError(t, err)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	data, err := io.ReadAll(res.Body)
+	require.NoError(t, err)
+	require.Equal(t, res.Status, fmt.Sprintf("%d %s", http.StatusOK, http.StatusText(http.StatusOK)))
+	require.Equal(t, string(data), fmt.Sprintf(`{"data":{"firstName":"%s","lastName":"%s"}}`, user.FirstName, user.LastName))
+}
+
+func TestMustEncodeResponse(t *testing.T) {
+	w := httptest.NewRecorder()
+	require.NotPanics(t, func() {
+		httpjson.EncodeResponse(w, http.StatusOK, nil)
+	})
+}
+
+func TestDecodeRequest(t *testing.T) {
+	var match testUser
+	user := newTestUser()
+
+	r, err := json.Marshal(user)
+	require.NoError(t, err)
+	body := strings.NewReader(string(r))
+
+	req := httptest.NewRequest(http.MethodGet, "/", body)
+
+	w := httptest.NewRecorder()
+	err = httpjson.DecodeRequest(w, req, &match)
+	require.NoError(t, err)
+	require.Equal(t, user, match)
+}
+
+type testUser struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+func newTestUser() testUser {
+	return testUser{
+		FirstName: gofakeit.FirstName(),
+		LastName:  gofakeit.LastName(),
+	}
+}
