a0db7d7d11b3c93ab6a12b68ae7e62a06424445c
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-14 13:44:30 +0800

    create tests for encoding http json error

---
+++ pkg/http/response/json/json_error_test.go
@@ -0,0 +1,81 @@
+package json_test
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/ssengalanto/potato-project/pkg/errors"
+	httpjson "github.com/ssengalanto/potato-project/pkg/http/response/json"
+	"github.com/stretchr/testify/require"
+)
+
+func TestEncodeError(t *testing.T) {
+	testCases := []struct {
+		name      string
+		apperror  error
+		httperror int
+	}{
+		{
+			name:      "bad request",
+			apperror:  errors.ErrInvalid,
+			httperror: http.StatusBadRequest,
+		},
+		{
+			name:      "unauthorized",
+			apperror:  errors.ErrUnauthorized,
+			httperror: http.StatusUnauthorized,
+		},
+		{
+			name:      "forbidden",
+			apperror:  errors.ErrForbidden,
+			httperror: http.StatusForbidden,
+		},
+		{
+			name:      "not found",
+			apperror:  errors.ErrNotFound,
+			httperror: http.StatusNotFound,
+		},
+		{
+			name:      "request timeout",
+			apperror:  errors.ErrTimeout,
+			httperror: http.StatusRequestTimeout,
+		},
+		{
+			name:      "service unavailable",
+			apperror:  errors.ErrTemporaryDisabled,
+			httperror: http.StatusServiceUnavailable,
+		},
+		{
+			name:      "internal server error",
+			apperror:  errors.ErrInternal,
+			httperror: http.StatusInternalServerError,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			err := httpjson.EncodeError(w, tc.apperror)
+			require.NoError(t, err)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			data, err := io.ReadAll(res.Body)
+			require.NoError(t, err)
+			require.Equal(t, res.Status, fmt.Sprintf("%d %s", tc.httperror, http.StatusText(tc.httperror)))
+			require.Equal(t, string(data), fmt.Sprintf(`{"error":{"code":%d,"message":"%s"}}`, tc.httperror, http.StatusText(tc.httperror)))
+		})
+	}
+
+}
+
+func TestMustEncodeError(t *testing.T) {
+	apperror := errors.ErrInvalid
+	w := httptest.NewRecorder()
+	require.NotPanics(t, func() {
+		httpjson.MustEncodeError(w, apperror)
+	})
+}
