7c18ef3881bbc535ece019feef9f7748f0b37cb8
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-17 10:57:27 +0800

    update account repository interface

--- cmd/account/internal/domain/account/repository.go
+++ cmd/account/internal/domain/account/repository.go
@@ -2,6 +2,7 @@ package account
 
 import (
 	"context"
+	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/address"
 
 	"github.com/google/uuid"
 )
@@ -9,6 +10,7 @@ import (
 type Repository interface {
 	Exists(ctx context.Context, id uuid.UUID) (bool, error)
 	Create(ctx context.Context, entity Entity) (Entity, error)
+	CreatePersonAddresses(ctx context.Context, entities []address.Entity) ([]address.Entity, error)
 	FindByID(ctx context.Context, id uuid.UUID) (Entity, error)
 	FindByEmail(ctx context.Context, email string) (Entity, error)
 	Update(ctx context.Context, entity Entity) (Entity, error)
