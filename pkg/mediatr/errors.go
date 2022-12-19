e2f6ed815965eff2bf0b2bf66b34a66da0452c8d
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-19 12:53:42 +0800

    add pipeline behaviours in mediatr

--- pkg/mediatr/errors.go
+++ pkg/mediatr/errors.go
@@ -7,9 +7,11 @@ var (
 	// ErrRequestHandlerAlreadyExists is returned when a request handler already exists in the registry.
 	ErrRequestHandlerAlreadyExists = fmt.Errorf("request handler already exists")
 	// ErrRequestHandlerNotFound is returned when a request handler doesn't exist in the registry.
-	ErrRequestHandlerNotFound = fmt.Errorf("request handler already exists")
+	ErrRequestHandlerNotFound = fmt.Errorf("request handler not found")
 	// ErrNotificationHandlerAlreadyExists is returned when a notification handler already exists in the registry.
 	ErrNotificationHandlerAlreadyExists = fmt.Errorf("notification handler already exists")
+	// ErrPipelineBehaviourAlreadyExists is returned when a pipeline behaviour already exists in the registry.
+	ErrPipelineBehaviourAlreadyExists = fmt.Errorf("pipeline behaviour already exists")
 	// ErrInvalidRequest is returned when request is invalid format.
 	ErrInvalidRequest = fmt.Errorf("request is invalid")
 )
--- pkg/mediatr/mediatr.go
+++ pkg/mediatr/mediatr.go
@@ -3,16 +3,22 @@ package mediatr
 import (
 	"context"
 	"fmt"
+	"reflect"
+
+	"github.com/ahmetb/go-linq/v3"
 )
 
 type Mediatr struct {
 	requestHandlerRegistry      map[string]RequestHandler
 	notificationHandlerRegistry map[string]NotificationHandler
+	pipelineBehaviourRegistry   []PipelineBehavior
 }
 
 type Request interface {
 	Topic() string
 }
+
+type RequestHandlerFunc func() (any, error)
 
 type RequestHandler interface {
 	Topic() string
@@ -24,10 +30,15 @@ type NotificationHandler interface {
 	Handle(ctx context.Context, notification Request) error
 }
 
+type PipelineBehavior interface {
+	Handle(ctx context.Context, request any, next RequestHandlerFunc) (any, error)
+}
+
 func NewMediatr() *Mediatr {
 	return &Mediatr{
 		requestHandlerRegistry:      map[string]RequestHandler{},
 		notificationHandlerRegistry: map[string]NotificationHandler{},
+		pipelineBehaviourRegistry:   []PipelineBehavior{},
 	}
 }
 
@@ -55,12 +66,53 @@ func (m *Mediatr) RegisterNotificationHandler(handler NotificationHandler) error
 	return nil
 }
 
+func (m *Mediatr) RegisterPipelineBehaviour(behaviour PipelineBehavior) error {
+	behaviourType := reflect.TypeOf(behaviour)
+
+	exists := m.existsPipeType(behaviourType)
+	if exists {
+		return fmt.Errorf("%w: %s", ErrPipelineBehaviourAlreadyExists, behaviourType)
+	}
+
+	m.pipelineBehaviourRegistry = append(m.pipelineBehaviourRegistry, behaviour)
+	return nil
+}
+
 func (m *Mediatr) Send(ctx context.Context, request Request) (any, error) {
 	topic := request.Topic()
 
 	handler, ok := m.requestHandlerRegistry[topic]
 	if !ok {
 		return nil, fmt.Errorf("%w: %s", ErrRequestHandlerNotFound, topic)
+	}
+
+	if len(m.pipelineBehaviourRegistry) > 0 {
+		var reversedPipes = reversePipelineOrder(m.pipelineBehaviourRegistry)
+
+		var lastHandler RequestHandlerFunc = func() (any, error) {
+			return handler.Handle(ctx, request)
+		}
+
+		aggregateResult := linq.From(reversedPipes).AggregateWithSeedT(
+			lastHandler,
+			func(next RequestHandlerFunc, pipe PipelineBehavior) RequestHandlerFunc {
+				pipeValue := pipe
+				nexValue := next
+
+				var handlerFunc RequestHandlerFunc = func() (any, error) {
+					return pipeValue.Handle(ctx, request, nexValue)
+				}
+
+				return handlerFunc
+			})
+
+		v := aggregateResult.(RequestHandlerFunc) //nolint:errcheck //unnecessary
+		response, err := v()
+		if err != nil {
+			return nil, err
+		}
+
+		return response, nil
 	}
 
 	response, err := handler.Handle(ctx, request)
@@ -85,4 +137,24 @@ func (m *Mediatr) Publish(ctx context.Context, request Request) error {
 	}
 
 	return nil
+}
+
+func reversePipelineOrder(values []PipelineBehavior) []PipelineBehavior {
+	var reverseValues []PipelineBehavior
+
+	for i := len(values) - 1; i >= 0; i-- {
+		reverseValues = append(reverseValues, values[i])
+	}
+
+	return reverseValues
+}
+
+func (m *Mediatr) existsPipeType(p reflect.Type) bool {
+	for _, pipe := range m.pipelineBehaviourRegistry {
+		if reflect.TypeOf(pipe) == p {
+			return true
+		}
+	}
+
+	return false
 }
