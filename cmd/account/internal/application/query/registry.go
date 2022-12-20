916e01ebb0dcb7c5674fa71be9e5b4cecf1b7669
Ssen Galanto <ssengalanto.dev@gmail.com>
2022-12-20 14:16:29 +0800

    create account query registry

---
+++ cmd/account/internal/application/query/registry.go
@@ -0,0 +1,15 @@
+package query
+
+import (
+	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/account"
+	"github.com/ssengalanto/potato-project/pkg/interfaces"
+	"github.com/ssengalanto/potato-project/pkg/mediatr"
+)
+
+func RegisterHandlers(logger interfaces.Logger, repository account.Repository, mediator *mediatr.Mediatr) {
+	getAccountQueryHandler := NewGetAccountQueryHandler(logger, repository)
+	err := mediator.RegisterRequestHandler(getAccountQueryHandler)
+	if err != nil {
+		logger.Fatal(err.Error(), nil)
+	}
+}
