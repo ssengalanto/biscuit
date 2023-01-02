f2c3854c0222e94b7aecb58546899202bbea0d33
Ssen Galanto <ssengalanto.dev@gmail.com>
2023-01-02 14:13:50 +0800

    restructure address table schema

--- cmd/account/migrations/pgsql/000003_address_schema.up.sql
+++ cmd/account/migrations/pgsql/000003_address_schema.up.sql
@@ -3,16 +3,13 @@ DROP TABLE IF EXISTS address;
 CREATE TABLE "address" (
     "id" uuid NOT NULL,
     "person_id" uuid NOT NULL,
-    "place_id" varchar NOT NULL,
-    "address_line1" json NOT NULL,
-    "address_line2" json NULL,
-    "city" json NOT NULL,
-    "state" json NOT NULL,
-    "country" json NOT NULL,
-    "postal_code" json NOT NULL,
-    "formatted_address" varchar NOT NULL,
-    "lat" decimal NOT NULL,
-    "lng" decimal NOT NULL,
+    "street" varchar NOT NULL,
+    "unit" varchar NULL,
+    "city" varchar NOT NULL,
+    "district" varchar NOT NULL,
+    "state" varchar NOT NULL,
+    "country" varchar NOT NULL,
+    "postal_code" varchar NOT NULL,
     "created_at" timestamp NOT NULL DEFAULT NOW(),
     "updated_at" timestamp NOT NULL DEFAULT NOW(),
     CONSTRAINT address_pkey PRIMARY KEY (id),
@@ -20,10 +17,3 @@ CREATE TABLE "address" (
 );
 
 comment on column address.person_id is 'Many-to-one relationship with person table.';
-comment on column address.place_id is 'Place id originated from Google Geolocation API.';
-comment on column address.address_line1 is 'JSON column that contains short_name and long_name fields.';
-comment on column address.address_line2 is 'JSON column that contains short_name and long_name fields.';
-comment on column address.city is 'JSON column that contains short_name and long_name fields.';
-comment on column address.state is 'JSON column that contains short_name and long_name fields.';
-comment on column address.country is 'JSON column that contains short_name and long_name fields.';
-comment on column address.postal_code is 'JSON column that contains short_name and long_name fields.';
