73fb2387e2342f6c58378f6e68a02e95ad43de82
Ssen Galanto <ssengalanto.dev@gmail.com>
2023-01-03 13:39:13 +0800

    create update account command

---
+++ cmd/account/internal/application/command/v1/update_account.go
@@ -0,0 +1,29 @@
+package v1
+
+import (
+	"time"
+
+	"github.com/ssengalanto/potato-project/cmd/account/internal/interfaces/dto"
+)
+
+// UpdateAccountCommand contains required fields for updating account.
+type UpdateAccountCommand struct {
+	ID          string                         `json:"id"`
+	FirstName   *string                        `json:"firstName"`
+	LastName    *string                        `json:"lastName"`
+	Phone       *string                        `json:"phone"`
+	DateOfBirth *time.Time                     `json:"dateOfBirth"`
+	Locations   *[]dto.UpdateAddressRequestDto `json:"locations"`
+}
+
+// NewUpdateAccountCommand creates a new command for updating account.
+func NewUpdateAccountCommand(accountID string, input dto.UpdateAccountRequestDto) *UpdateAccountCommand {
+	return &UpdateAccountCommand{
+		ID:          accountID,
+		FirstName:   input.FirstName,
+		LastName:    input.LastName,
+		Phone:       input.Phone,
+		DateOfBirth: input.DateOfBirth,
+		Locations:   input.Locations,
+	}
+}
