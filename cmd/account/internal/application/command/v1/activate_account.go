46e886b073a5bfae214cd2d8408466d973709169
Ssen Galanto <ssengalanto.dev@gmail.com>
2023-01-04 14:30:50 +0800

    create activate account command

---
+++ cmd/account/internal/application/command/v1/activate_account.go
@@ -0,0 +1,17 @@
+package v1
+
+import (
+	"github.com/ssengalanto/potato-project/cmd/account/internal/interfaces/dto"
+)
+
+// ActivateAccountCommand contains required fields for account activation.
+type ActivateAccountCommand struct {
+	ID string `json:"id"`
+}
+
+// NewActivateAccountCommand creates a new command for account activation.
+func NewActivateAccountCommand(input dto.ActivateAccountRequestDto) *ActivateAccountCommand {
+	return &ActivateAccountCommand{
+		ID: input.ID,
+	}
+}
