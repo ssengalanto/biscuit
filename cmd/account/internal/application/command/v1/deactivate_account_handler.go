37cb91c7142a12d55e0fe7102c6cc1f4c423b46d
Ssen Galanto <ssengalanto.dev@gmail.com>
2023-01-04 15:06:00 +0800

    implement deactivate account command handler

---
+++ cmd/account/internal/application/command/v1/deactivate_account_handler.go
@@ -0,0 +1,77 @@
+package v1
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/ssengalanto/potato-project/cmd/account/internal/domain/account"
+	"github.com/ssengalanto/potato-project/cmd/account/internal/interfaces/dto"
+	"github.com/ssengalanto/potato-project/pkg/errors"
+	"github.com/ssengalanto/potato-project/pkg/interfaces"
+)
+
+// DeactivateAccountCommandHandler - command handler struct for account deactivation, satisfies mediatr.RequestHandler.
+type DeactivateAccountCommandHandler struct {
+	log               interfaces.Logger
+	accountRepository account.Repository
+	cache             account.Cache
+}
+
+// NewDeactivateAccountCommandHandler creates a new command handler that handles account deactivation.
+func NewDeactivateAccountCommandHandler(
+	logger interfaces.Logger,
+	accountRepository account.Repository,
+	cache account.Cache,
+) *DeactivateAccountCommandHandler {
+	return &DeactivateAccountCommandHandler{
+		log:               logger,
+		accountRepository: accountRepository,
+		cache:             cache,
+	}
+}
+
+func (d *DeactivateAccountCommandHandler) Name() string {
+	return fmt.Sprintf("%T", &DeactivateAccountCommand{})
+}
+
+func (d *DeactivateAccountCommandHandler) Handle(
+	ctx context.Context,
+	request any,
+) (any, error) {
+	empty := dto.DeactivateAccountResponseDto{}
+
+	command, ok := request.(*DeactivateAccountCommand)
+	if !ok {
+		d.log.Error("invalid command", map[string]any{"command": command})
+		return empty, fmt.Errorf("%w: command", errors.ErrInvalid)
+	}
+	d.log.Info(fmt.Sprintf("executing %s", d.Name()), nil)
+
+	id, err := uuid.Parse(command.ID)
+	if err != nil {
+		d.log.Error("invalid uuid", map[string]any{"command": command, "error": err})
+		return empty, fmt.Errorf("%w: uuid %s", errors.ErrInvalid, command.ID)
+	}
+
+	acct, err := d.accountRepository.FindByID(ctx, id)
+	if err != nil {
+		return empty, err
+	}
+
+	acct.Deactivate()
+
+	err = d.accountRepository.Update(ctx, acct)
+	if err != nil {
+		return empty, err
+	}
+
+	err = d.cache.Delete(ctx, command.ID)
+	if err != nil {
+		return empty, err
+	}
+
+	response := dto.DeactivateAccountResponseDto{ID: command.ID}
+
+	return response, err
+}
