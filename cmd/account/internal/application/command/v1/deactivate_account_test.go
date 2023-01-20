e46f8bf5d33e2a895db7e6c8581deeaf84373938
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-01-20 20:26:34 +0800

    create tests for deactivate account command request

---
+++ cmd/account/internal/application/command/v1/deactivate_account_test.go
@@ -0,0 +1,92 @@
+package v1_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/golang/mock/gomock"
+	"github.com/google/uuid"
+	v1 "github.com/ssengalanto/biscuit/cmd/account/internal/application/command/v1"
+	"github.com/ssengalanto/biscuit/cmd/account/internal/interfaces/dto"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewDeactivateAccountCommand(t *testing.T) {
+	t.Parallel()
+	t.Run("it should create a new deactivate account command instance", func(t *testing.T) {
+		t.Parallel()
+		input := dto.DeactivateAccountRequest{ID: gofakeit.UUID()}
+		cmd := v1.NewDeactivateAccountCommand(input)
+		assert.NotNil(t, cmd)
+	})
+}
+
+func TestNewDeactivateAccountCommandHandler(t *testing.T) {
+	t.Parallel()
+	t.Run("it should create a new deactivate account handler instance", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		logger, repository, cache := createDepedencies(ctrl)
+		hdlr := v1.NewDeactivateAccountCommandHandler(logger, repository, cache)
+		assert.NotNil(t, hdlr)
+	})
+}
+
+func TestDeactivateAccountCommandHandler_Name(t *testing.T) {
+	t.Parallel()
+	t.Run("it should return the correct handler name", func(t *testing.T) {
+		t.Parallel()
+		ctrl := gomock.NewController(t)
+		logger, repository, cache := createDepedencies(ctrl)
+		hdlr := v1.NewDeactivateAccountCommandHandler(logger, repository, cache)
+		n := hdlr.Name()
+		assert.Equal(t, fmt.Sprintf("%T", &v1.DeactivateAccountCommand{}), n)
+	})
+}
+
+func TestDeactivateAccountCommandHandler_Handle(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	logger, repository, cache := createDepedencies(ctrl)
+	hdlr := v1.NewDeactivateAccountCommandHandler(logger, repository, cache)
+
+	t.Run("it should return the correct response", func(t *testing.T) {
+		t.Parallel()
+		id := uuid.New()
+		repository.EXPECT().FindByID(ctx, gomock.Eq(id))
+		repository.EXPECT().Update(ctx, gomock.Any())
+		cache.EXPECT().Delete(ctx, gomock.Eq(id.String()))
+		res, err := hdlr.Handle(ctx, &v1.DeactivateAccountCommand{ID: id.String()})
+		require.NoError(t, err)
+
+		r, ok := res.(dto.DeactivateAccountResponse)
+		assert.True(t, ok)
+		assert.Equal(t, id.String(), r.ID)
+	})
+	t.Run("it should return an error when an invalid uuid is provided", func(t *testing.T) {
+		t.Parallel()
+		id := "invalid"
+		logger.EXPECT().Error(gomock.Any(), gomock.Any())
+		res, err := hdlr.Handle(ctx, &v1.DeactivateAccountCommand{ID: id})
+		require.Error(t, err)
+
+		r, ok := res.(dto.DeactivateAccountResponse)
+		assert.True(t, ok)
+		assert.Equal(t, "", r.ID)
+	})
+	t.Run("it should return an error when an invalid command is provided", func(t *testing.T) {
+		t.Parallel()
+		req := struct{}{}
+		logger.EXPECT().Error(gomock.Any(), gomock.Any())
+		res, err := hdlr.Handle(ctx, &req)
+		require.Error(t, err)
+
+		r, ok := res.(dto.DeactivateAccountResponse)
+		assert.True(t, ok)
+		assert.Equal(t, "", r.ID)
+	})
+}
