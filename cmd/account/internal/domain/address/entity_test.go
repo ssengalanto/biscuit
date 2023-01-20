a6ed99258f2919ba339ade2a81a27e8b3e29e620
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-01-20 13:20:50 +0800

    update tests for address entity and value objects

--- cmd/account/internal/domain/address/entity_test.go
+++ cmd/account/internal/domain/address/entity_test.go
@@ -1,60 +1,68 @@
 package address_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/google/uuid"
 	"github.com/ssengalanto/biscuit/cmd/account/internal/domain/address"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestNew(t *testing.T) {
-	entity := newAddress()
-	require.NotNilf(t, entity, "entity should not be nil")
+	t.Parallel()
+	t.Run("it should create a new address", func(t *testing.T) {
+		t.Parallel()
+		entity := newAddress()
+		assert.NotNil(t, entity)
+	})
 }
 
 func TestEntity_UpdateComponents(t *testing.T) {
-	testCases := []struct {
-		name   string
-		assert func(t *testing.T)
-	}{
-		{
-			name: "update address success",
-			assert: func(t *testing.T) {
-				addr := gofakeit.Address()
-				payload := address.UpdateComponentsInput{
-					Street:     &addr.Address,
-					Unit:       &addr.Address,
-					City:       &addr.City,
-					District:   &addr.City,
-					State:      &addr.State,
-					Country:    &addr.Country,
-					PostalCode: &addr.Zip,
-				}
-
-				entity := newAddress()
-				err := entity.UpdateComponents(payload)
-				errMsg := fmt.Sprintf("update address should succeed: %s", err)
-				require.Equal(t, entity.Components, address.Components{
-					Street:     *payload.Street,
-					Unit:       *payload.Unit,
-					City:       *payload.City,
-					District:   *payload.District,
-					State:      *payload.State,
-					Country:    *payload.Country,
-					PostalCode: *payload.PostalCode,
-				}, errMsg)
-				require.Nil(t, err, errMsg)
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.assert(t)
+	t.Parallel()
+	t.Run("it should update the address successfully", func(t *testing.T) {
+		t.Parallel()
+		addr := gofakeit.Address()
+		payload := address.UpdateComponentsInput{
+			Street:     &addr.Address,
+			Unit:       &addr.Address,
+			City:       &addr.City,
+			District:   &addr.City,
+			State:      &addr.State,
+			Country:    &addr.Country,
+			PostalCode: &addr.Zip,
+		}
+		entity := newAddress()
+		err := entity.UpdateComponents(payload)
+		assert.Equal(t, entity.Components, address.Components{
+			Street:     *payload.Street,
+			Unit:       *payload.Unit,
+			City:       *payload.City,
+			District:   *payload.District,
+			State:      *payload.State,
+			Country:    *payload.Country,
+			PostalCode: *payload.PostalCode,
 		})
-	}
+		require.NoError(t, err)
+	})
+}
+
+func Test_IsValid(t *testing.T) {
+	t.Parallel()
+	t.Run("it should be a valid address", func(t *testing.T) {
+		t.Parallel()
+		entity := newAddress()
+		err := entity.IsValid()
+		require.NoError(t, err)
+	})
+	t.Run("it should be an invalid address", func(t *testing.T) {
+		t.Parallel()
+		entity := newAddress()
+		entity.Components.Street = ""
+		err := entity.IsValid()
+		require.Error(t, err)
+	})
 }
 
 func newAddress() address.Entity {
--- cmd/account/internal/domain/address/value_object_test.go
+++ cmd/account/internal/domain/address/value_object_test.go
@@ -1,99 +1,98 @@
 package address_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/ssengalanto/biscuit/cmd/account/internal/domain/address"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestComponents_IsValid(t *testing.T) {
-	addr := gofakeit.Address()
-	testCases := []struct {
-		name    string
-		payload address.Components
-		assert  func(t *testing.T, err error)
-	}{
-		{
-			name: "valid address components",
-			payload: address.Components{
-				Street:     addr.Address,
-				Unit:       addr.Address,
-				City:       addr.City,
-				District:   addr.City,
-				State:      addr.State,
-				Country:    addr.Country,
-				PostalCode: addr.Zip,
-			},
-			assert: func(t *testing.T, err error) {
-				errMsg := fmt.Sprintf("address components should be valid: %s", err)
-				require.Nil(t, err, errMsg)
-			},
-		},
-		{
-			name:    "invalid address components",
-			payload: address.Components{},
-			assert: func(t *testing.T, err error) {
-				errMsg := fmt.Sprintf("address components should be invalid: %s", err)
-				require.NotNil(t, err, errMsg)
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			components := tc.payload
-			err := components.IsValid()
-			tc.assert(t, err)
-		})
-	}
+	t.Parallel()
+	t.Run("it should be a valid address components", func(t *testing.T) {
+		t.Parallel()
+		addr := gofakeit.Address()
+		components := address.Components{
+			Street:     addr.Address,
+			Unit:       addr.Address,
+			City:       addr.City,
+			District:   addr.City,
+			State:      addr.State,
+			Country:    addr.Country,
+			PostalCode: addr.Zip,
+		}
+		err := components.IsValid()
+		require.NoError(t, err)
+	})
+	t.Run("it should be an invalid address components", func(t *testing.T) {
+		t.Parallel()
+		components := address.Components{}
+		err := components.IsValid()
+		assert.NotNil(t, err)
+		require.Error(t, err)
+	})
 }
 
 func TestAddress_Update(t *testing.T) {
-	current := gofakeit.Address()
-	update := gofakeit.Address()
+	t.Parallel()
+	t.Run("it should update the address components successfully", func(t *testing.T) {
+		t.Parallel()
 
-	currentAddr := address.Components{
-		Street:     current.Address,
-		Unit:       current.Address,
-		City:       current.City,
-		District:   current.City,
-		State:      current.State,
-		Country:    current.Country,
-		PostalCode: current.Zip,
-	}
+		current := gofakeit.Address()
+		components := address.Components{
+			Street:     current.Address,
+			Unit:       current.Address,
+			City:       current.City,
+			District:   current.City,
+			State:      current.State,
+			Country:    current.Country,
+			PostalCode: current.Zip,
+		}
 
-	testCases := []struct {
-		name    string
-		current address.Components
-		update  address.Components
-		assert  func(t *testing.T, expected address.Components, actual address.Components, err error)
-	}{
-		{
-			name:    "update address components success",
-			current: currentAddr,
-			update: address.Components{
-				Street:     update.Address,
-				Unit:       update.Address,
-				City:       update.City,
-				District:   update.City,
-				State:      update.State,
-				Country:    update.Country,
-				PostalCode: update.Zip,
-			},
-			assert: func(t *testing.T, expected address.Components, actual address.Components, err error) {
-				errMsg := fmt.Sprintf("update address components should succeed: %s", err)
-				require.Equal(t, expected, actual, errMsg)
-				require.Nil(t, err, errMsg)
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			components := tc.current
-			newComponents, err := components.Update(tc.update)
-			tc.assert(t, tc.update, newComponents, err)
-		})
-	}
+		update := gofakeit.Address()
+		payload := address.Components{
+			Street:     update.Address,
+			Unit:       update.Address,
+			City:       update.City,
+			District:   update.City,
+			State:      update.State,
+			Country:    update.Country,
+			PostalCode: update.Zip,
+		}
+
+		newComponents, err := components.Update(payload)
+		assert.Equal(t, payload, newComponents)
+		require.NoError(t, err)
+	})
+	t.Run("it should fail to update the address components", func(t *testing.T) {
+		t.Parallel()
+
+		current := gofakeit.Address()
+		components := address.Components{
+			Street:     current.Address,
+			Unit:       current.Address,
+			City:       current.City,
+			District:   current.City,
+			State:      current.State,
+			Country:    current.Country,
+			PostalCode: current.Zip,
+		}
+
+		update := gofakeit.Address()
+		payload := address.Components{
+			Street:     "",
+			Unit:       update.Address,
+			City:       update.City,
+			District:   update.City,
+			State:      update.State,
+			Country:    update.Country,
+			PostalCode: update.Zip,
+		}
+
+		newComponents, err := components.Update(payload)
+		assert.Equal(t, address.Components{}, newComponents)
+		require.Error(t, err)
+	})
 }
