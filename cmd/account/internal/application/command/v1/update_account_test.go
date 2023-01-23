f20042cf39df8ba22271fa6395d57c7db7e64b05
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-01-23 22:29:22 +0800

    resolve update account correct response test case

--- cmd/account/internal/application/command/v1/update_account_test.go
+++ cmd/account/internal/application/command/v1/update_account_test.go
@@ -8,7 +8,9 @@ import (
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/golang/mock/gomock"
 	v1 "github.com/ssengalanto/biscuit/cmd/account/internal/application/command/v1"
+	"github.com/ssengalanto/biscuit/cmd/account/internal/domain/account"
 	"github.com/ssengalanto/biscuit/cmd/account/internal/interfaces/dto"
+	"github.com/ssengalanto/biscuit/cmd/account/internal/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -17,8 +19,7 @@ func TestNewUpdateAccountCommand(t *testing.T) {
 	t.Parallel()
 	t.Run("it should create a new update account command instance", func(t *testing.T) {
 		t.Parallel()
-		req := newUpdateAccountRequest()
-		cmd := v1.NewUpdateAccountCommand(gofakeit.UUID(), req)
+		cmd := v1.NewUpdateAccountCommand(gofakeit.UUID(), dto.UpdateAccountRequest{})
 		assert.NotNil(t, cmd)
 	})
 }
@@ -48,23 +49,25 @@ func TestUpdateAccountCommandHandler_Handle(t *testing.T) {
 	logger, repository, cache := createDepedencies(ctrl)
 	hdlr := v1.NewUpdateAccountCommandHandler(logger, repository, cache)
 
-	// t.Run("it should return the correct response", func(t *testing.T) {
-	//	req := newUpdateAccountRequest()
-	//	repository.EXPECT().FindByID(ctx, gomock.Any())
-	//	repository.EXPECT().Update(ctx, gomock.Any())
-	//	cache.EXPECT().Delete(ctx, gomock.Any())
-	//	res, err := hdlr.Handle(ctx, &v1.UpdateAccountCommand{
-	//		ID:          gofakeit.UUID(),
-	//		FirstName:   req.FirstName,
-	//		LastName:    req.LastName,
-	//		Phone:       req.Phone,
-	//		DateOfBirth: req.DateOfBirth,
-	//	})
-	//	require.NoError(t, err)
-	//
-	//	_, ok := res.(dto.UpdateAccountResponse)
-	//	assert.True(t, ok)
-	// })
+	t.Run("it should return the correct response", func(t *testing.T) {
+		entity, req := newUpdateAccountRequest()
+		repository.EXPECT().FindByID(ctx, gomock.Any()).Return(entity, nil)
+		repository.EXPECT().Update(ctx, gomock.Any())
+		cache.EXPECT().Delete(ctx, gomock.Any())
+		res, err := hdlr.Handle(ctx, &v1.UpdateAccountCommand{
+			ID:          entity.ID.String(),
+			FirstName:   req.FirstName,
+			LastName:    req.LastName,
+			Phone:       req.Phone,
+			DateOfBirth: req.DateOfBirth,
+			Locations:   req.Locations,
+		})
+		require.NoError(t, err)
+
+		r, ok := res.(dto.UpdateAccountResponse)
+		assert.True(t, ok)
+		assert.Equal(t, entity.ID.String(), r.ID)
+	})
 	t.Run("it should return an error when an invalid uuid is provided", func(t *testing.T) {
 		id := "invalid"
 		logger.EXPECT().Error(gomock.Any(), gomock.Any())
@@ -87,13 +90,30 @@ func TestUpdateAccountCommandHandler_Handle(t *testing.T) {
 	})
 }
 
-func newUpdateAccountRequest() dto.UpdateAccountRequest {
+func newUpdateAccountRequest() (account.Entity, dto.UpdateAccountRequest) {
+	var loc []dto.UpdateAddressRequest
+	entity := mock.NewAccountEntity()
 	fn := gofakeit.FirstName()
 	ln := gofakeit.LastName()
 	phone := gofakeit.Phone()
 	dob := gofakeit.Date()
-	loc := []dto.UpdateAddressRequest{}
-	return dto.UpdateAccountRequest{
+
+	for _, addr := range *entity.Person.Address {
+		a := gofakeit.Address()
+		req := dto.UpdateAddressRequest{
+			ID:         addr.ID.String(),
+			Street:     &a.Street,
+			Unit:       &a.Street,
+			City:       &a.City,
+			District:   &a.City,
+			State:      &a.State,
+			Country:    &a.Country,
+			PostalCode: &a.Zip,
+		}
+		loc = append(loc, req)
+	}
+
+	return entity, dto.UpdateAccountRequest{
 		FirstName:   &fn,
 		LastName:    &ln,
 		Phone:       &phone,
