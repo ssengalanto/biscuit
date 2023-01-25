e3c172004354ce7d44e49c809af0c8ce120db7a8
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-01-25 15:41:34 +0800

    create test setup, updates existing test cases

--- cmd/account/internal/interfaces/http/handlers/v1/deactivate_account_test.go
+++ cmd/account/internal/interfaces/http/handlers/v1/deactivate_account_test.go
@@ -7,62 +7,47 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	"github.com/go-chi/chi/v5"
 	"github.com/golang/mock/gomock"
 	cmdv1 "github.com/ssengalanto/biscuit/cmd/account/internal/application/command/v1"
 	qv1 "github.com/ssengalanto/biscuit/cmd/account/internal/application/query/v1"
-	v1 "github.com/ssengalanto/biscuit/cmd/account/internal/interfaces/http/handlers/v1"
 	acctmock "github.com/ssengalanto/biscuit/cmd/account/internal/mock"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewDeactivateAccountHandler(t *testing.T) {
-	t.Run("it should create a new deactivate account http handler", func(t *testing.T) {
-		logger, mediator := createDependencies(t)
-		hdlr := v1.NewDeactivateAccountHandler(logger, mediator)
-		assert.NotNil(t, hdlr)
-	})
-}
-
 func TestDeactivateAccountHandler_Handle(t *testing.T) {
-	logger, mediator := createDependencies(t)
-	hdlr := v1.NewDeactivateAccountHandler(logger, mediator)
-	assert.NotNil(t, hdlr)
-
-	mux := chi.NewRouter()
-	mux.Patch("/api/v1/accounts/{id}/deactivate", hdlr.Handle)
+	s := setup(t)
 
 	acct := acctmock.NewAccountEntity()
 	id := acct.ID.String()
 	url := fmt.Sprintf("/api/v1/accounts/%s/deactivate", id)
 
 	t.Run("it should return success response", func(t *testing.T) {
-		mediator.EXPECT().Send(gomock.Any(), &cmdv1.DeactivateAccountCommand{ID: id}).Times(1)
-		mediator.EXPECT().Send(gomock.Any(), &qv1.GetAccountQuery{ID: id}).Times(1).Return(acct, nil)
+		s.mediator.EXPECT().Send(gomock.Any(), &cmdv1.DeactivateAccountCommand{ID: id}).Times(1)
+		s.mediator.EXPECT().Send(gomock.Any(), &qv1.GetAccountQuery{ID: id}).Times(1).Return(acct, nil)
 
 		r, err := http.NewRequest(http.MethodPatch, url, nil)
 		require.NoError(t, err)
 
 		w := httptest.NewRecorder()
-		mux.ServeHTTP(w, r)
+		s.mux.ServeHTTP(w, r)
 
 		assert.Equal(t, http.StatusOK, w.Code)
 	})
 	t.Run("it should return an error due to an invalid uuid", func(t *testing.T) {
-		logger.EXPECT().Error(gomock.Any(), gomock.Any()).Times(1)
+		s.logger.EXPECT().Error(gomock.Any(), gomock.Any()).Times(1)
 
 		invalid := fmt.Sprintf("/api/v1/accounts/%s/deactivate", "invalid")
 		r, err := http.NewRequest(http.MethodPatch, invalid, nil)
 		require.NoError(t, err)
 
 		w := httptest.NewRecorder()
-		mux.ServeHTTP(w, r)
+		s.mux.ServeHTTP(w, r)
 
 		assert.Equal(t, http.StatusBadRequest, w.Code)
 	})
 	t.Run("it should return an error due to deactivate account command failure", func(t *testing.T) {
-		mediator.EXPECT().Send(
+		s.mediator.EXPECT().Send(
 			gomock.Any(),
 			&cmdv1.DeactivateAccountCommand{ID: id},
 		).Times(1).Return(nil, errors.New("error"))
@@ -71,13 +56,13 @@ func TestDeactivateAccountHandler_Handle(t *testing.T) {
 		require.NoError(t, err)
 
 		w := httptest.NewRecorder()
-		mux.ServeHTTP(w, r)
+		s.mux.ServeHTTP(w, r)
 
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
 	t.Run("it should return an error due to get account query failure", func(t *testing.T) {
-		mediator.EXPECT().Send(gomock.Any(), &cmdv1.DeactivateAccountCommand{ID: id}).Times(1)
-		mediator.EXPECT().Send(
+		s.mediator.EXPECT().Send(gomock.Any(), &cmdv1.DeactivateAccountCommand{ID: id}).Times(1)
+		s.mediator.EXPECT().Send(
 			gomock.Any(),
 			&qv1.GetAccountQuery{ID: id},
 		).Times(1).Return(nil, errors.New("error"))
@@ -86,7 +71,7 @@ func TestDeactivateAccountHandler_Handle(t *testing.T) {
 		require.NoError(t, err)
 
 		w := httptest.NewRecorder()
-		mux.ServeHTTP(w, r)
+		s.mux.ServeHTTP(w, r)
 
 		assert.Equal(t, http.StatusInternalServerError, w.Code)
 	})
--- cmd/account/internal/interfaces/http/handlers/v1/helpers_test.go
+++ cmd/account/internal/interfaces/http/handlers/v1/helpers_test.go
@@ -1,15 +1,55 @@
 package v1_test
 
 import (
+	"encoding/json"
+	"io"
+	"strings"
 	"testing"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/golang/mock/gomock"
+	v1 "github.com/ssengalanto/biscuit/cmd/account/internal/interfaces/http/handlers/v1"
 	"github.com/ssengalanto/biscuit/pkg/mock"
 )
 
-func createDependencies(t *testing.T) (*mock.MockLogger, *mock.MockMediator) {
+type testSetup struct {
+	mux      *chi.Mux
+	logger   *mock.MockLogger
+	mediator *mock.MockMediator
+}
+
+func setup(t *testing.T) *testSetup {
 	ctrl := gomock.NewController(t)
 	logger := mock.NewMockLogger(ctrl)
 	mediator := mock.NewMockMediator(ctrl)
-	return logger, mediator
+
+	getAccountHandler := v1.NewGetAccountHandler(logger, mediator)
+	createAccountHandler := v1.NewCreateAccountHandler(logger, mediator)
+	activateAccountHandler := v1.NewActivateAccountHandler(logger, mediator)
+	deactivateAccountHandler := v1.NewDeactivateAccountHandler(logger, mediator)
+	updateAccountHandler := v1.NewUpdateAccountHandler(logger, mediator)
+	deleteAccountHandler := v1.NewDeleteAccountHandler(logger, mediator)
+
+	mux := chi.NewRouter()
+	mux.Get("/api/v1/accounts/{id}", getAccountHandler.Handle)
+	mux.Post("/api/v1/accounts", createAccountHandler.Handle)
+	mux.Patch("/api/v1/accounts/{id}", updateAccountHandler.Handle)
+	mux.Patch("/api/v1/accounts/{id}/activate", activateAccountHandler.Handle)
+	mux.Patch("/api/v1/accounts/{id}/deactivate", deactivateAccountHandler.Handle)
+	mux.Delete("/api/v1/accounts/{id}", deleteAccountHandler.Handle)
+
+	return &testSetup{
+		mux:      mux,
+		logger:   logger,
+		mediator: mediator,
+	}
+}
+
+func parsePayload(payload any) io.Reader {
+	res, err := json.Marshal(payload)
+	if err != nil {
+		panic(err)
+	}
+
+	return strings.NewReader(string(res))
 }
