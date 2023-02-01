2e210d7648ed39de0671ce4b9d88add48bd909a4
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-02-01 14:37:07 +0800

    update token entity

--- cmd/auth/internal/domain/token/entity.go
+++ cmd/auth/internal/domain/token/entity.go
@@ -7,23 +7,30 @@ import (
 )
 
 const (
-	tokenExpiry = 15 * time.Minute
+	TokenExpiry = 15 * time.Minute
 )
 
+// Entity - token entity struct.
 type Entity struct {
 	ID        uuid.UUID `json:"id" validate:"required"`
 	AccountID string    `json:"account_id" validate:"required"`
-	ClientID  string    `json:"client_id"`
-	ExpiredAt time.Time `json:"expired_at"`
+	ClientID  string    `json:"client_id" validate:"required"`
+	ExpiresIn time.Time `json:"expires_in" validate:"required"`
+}
+
+// Subject struct contains required fields for token claims.
+type Subject struct {
+	AccountID string
+	Email     string
+	ClientID  string
+	Issuer    string
 }
 
 // New creates a new token entity.
 func New(accountID, clientID string) Entity {
-	now := time.Now()
 	return Entity{
 		ID:        uuid.New(),
 		AccountID: accountID,
 		ClientID:  clientID,
-		ExpiredAt: now.Add(tokenExpiry),
 	}
 }
