396f05ce310050e6e5bc492d10fc21262519dc55
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-02-01 14:08:04 +0800

    create base64 rsa private and public key value objects

--- cmd/auth/internal/domain/token/errors.go
+++ cmd/auth/internal/domain/token/errors.go
@@ -6,4 +6,18 @@ import "fmt"
 var (
 	// ErrInvalidGrantType is returned when the grant type is invalid.
 	ErrInvalidGrantType = fmt.Errorf("invalid grant type")
+	// ErrInvalidRSAPrivateKey is returned when the rsa private key is invalid.
+	ErrInvalidRSAPrivateKey = fmt.Errorf("invalid rsa private key")
+	// ErrParseRSAPrivateKeyFailed is returned when parsing rsa private key failed.
+	ErrParseRSAPrivateKeyFailed = fmt.Errorf("invalid rsa private key")
+	// ErrInvalidRSAPublicKey is returned when the rsa public key is invalid.
+	ErrInvalidRSAPublicKey = fmt.Errorf("invalid rsa public key")
+	// ErrParseRSAPublicKeyFailed is returned when parsing rsa public key failed.
+	ErrParseRSAPublicKeyFailed = fmt.Errorf("invalid rsa public key")
+	// ErrInvalidJWTToken is returned when the JWT token is invalid.
+	ErrInvalidJWTToken = fmt.Errorf("invalid jwt token")
+	// ErrInvalidSigningMethod is returned when unexpected method is used for signing JWT token.
+	ErrInvalidSigningMethod = fmt.Errorf("unexpected method")
+	// ErrSigningTokenFailed is returned when signing token failed.
+	ErrSigningTokenFailed = fmt.Errorf("signing token failed")
 )
--- cmd/auth/internal/domain/token/value_objects.go
+++ cmd/auth/internal/domain/token/value_objects.go
@@ -1,6 +1,10 @@
 package token
 
 import (
+	"crypto/rsa"
+	"encoding/base64"
+
+	"github.com/golang-jwt/jwt"
 	"github.com/ssengalanto/biscuit/pkg/validator"
 )
 
@@ -8,8 +12,52 @@ const (
 	GrantTypePassword = "grant-type-password"
 )
 
-var grantTypes = map[string]string{ //nolint:gochecknoglobals //intendedgit pu
+var grantTypes = map[string]string{ //nolint:gochecknoglobals //intended
 	GrantTypePassword: "password",
+}
+
+type Base64RSAPrivateKey string
+
+// Parse decodes the base64 RSA private key and parse it to PKCS1 or PKCS8.
+func (b Base64RSAPrivateKey) Parse() (*rsa.PrivateKey, error) {
+	pk, err := base64.StdEncoding.DecodeString(b.String())
+	if err != nil {
+		return nil, ErrInvalidRSAPrivateKey
+	}
+
+	key, err := jwt.ParseRSAPrivateKeyFromPEM(pk)
+	if err != nil {
+		return nil, ErrParseRSAPrivateKeyFailed
+	}
+
+	return key, nil
+}
+
+// String converts the Base64RSAPrivateKey to string format.
+func (b Base64RSAPrivateKey) String() string {
+	return string(b)
+}
+
+type Base64RSAPublicKey string
+
+// Parse decodes the base64 RSA public key and parse it to PKCS1 or PKCS8.
+func (b Base64RSAPublicKey) Parse() (*rsa.PublicKey, error) {
+	pk, err := base64.StdEncoding.DecodeString(b.String())
+	if err != nil {
+		return nil, ErrInvalidRSAPublicKey
+	}
+
+	key, err := jwt.ParseRSAPublicKeyFromPEM(pk)
+	if err != nil {
+		return nil, ErrParseRSAPublicKeyFailed
+	}
+
+	return key, nil
+}
+
+// String converts the Base64RSAPublicKey to string format.
+func (b Base64RSAPublicKey) String() string {
+	return string(b)
 }
 
 type GrantType string
