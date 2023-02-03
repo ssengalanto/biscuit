f9688be5bf5cc4b6635a339f1f9e3e3537f7cdd0
Ssen Galanto <ssengalanto.sde@gmail.com>
2023-02-03 20:48:09 +0800

    fix typo errors

--- cmd/auth/internal/domain/token/errors.go
+++ cmd/auth/internal/domain/token/errors.go
@@ -9,11 +9,11 @@ var (
 	// ErrInvalidRSAPrivateKey is returned when the rsa private key is invalid.
 	ErrInvalidRSAPrivateKey = fmt.Errorf("invalid rsa private key")
 	// ErrParseRSAPrivateKeyFailed is returned when parsing rsa private key failed.
-	ErrParseRSAPrivateKeyFailed = fmt.Errorf("invalid rsa private key")
+	ErrParseRSAPrivateKeyFailed = fmt.Errorf("parsing rsa private key failed")
 	// ErrInvalidRSAPublicKey is returned when the rsa public key is invalid.
 	ErrInvalidRSAPublicKey = fmt.Errorf("invalid rsa public key")
 	// ErrParseRSAPublicKeyFailed is returned when parsing rsa public key failed.
-	ErrParseRSAPublicKeyFailed = fmt.Errorf("invalid rsa public key")
+	ErrParseRSAPublicKeyFailed = fmt.Errorf("parsing rsa public failed")
 	// ErrInvalidJWTToken is returned when the JWT token is invalid.
 	ErrInvalidJWTToken = fmt.Errorf("invalid jwt token")
 	// ErrInvalidSigningMethod is returned when unexpected method is used for signing JWT token.
